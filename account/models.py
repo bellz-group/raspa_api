6e5bd37d8c1018d7b69a5a637ae06dd2cf194e79
unknown <olaniyigeorge77@gmail.com>
2024-01-11 23:29:25 +0100

    User models

--- account/models.py
+++ account/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, PermissionsMixin, Group, Permission
+import uuid
 
-# Create your models here.
+
+
+
+class BaseUser(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    # Details    
+    email = models.EmailField(unique=True, null=False, blank=False)
+    
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+
+
+     # Add related_name to avoid clashes
+    # groups = models.ManyToManyField(Group, related_name='baseuser_groups')
+    # user_permissions = models.ManyToManyField(Permission, related_name='baseuser_perms')
+
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class BaseUserProfile(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    user = models.ForeignKey(BaseUser, on_delete=models.CASCADE, related_name="user_profile")
+
+
--- raspa_api/settings.py
+++ raspa_api/settings.py
@@ -135,6 +135,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # -----------------------------------------------
 
+AUTH_USER_MODEL = 'account.BaseUser'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
