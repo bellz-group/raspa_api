ce6adce35947569c256e5c0fc405d8e46caba001
unknown <olaniyigeorge77@gmail.com>
2024-03-03 14:43:14 +0100

    Filtering

--- api/views.py
+++ api/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import generics 
+from rest_framework import generics
+from django_filters.rest_framework import DjangoFilterBackend
+from api.filters import DevdPropertyFilter 
 from .models import *
 from .serializers import *
-
+from rest_framework import filters
 
 class Index(APIView):
     def get(self, request, format=None):
@@ -19,7 +21,10 @@ class DevelopedPropertyListCreateViewSet(generics.ListCreateAPIView):
     
     queryset = DevelopedProperty.objects.all()
     serializer_class = DevelopedPropertySerializer
-    lookup_field = "id"
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
+    filterset_class = DevdPropertyFilter
+    filterset_fields = ('size', 'type', 'bdrs', 'flrs')
+    search_fields = ['property_name', 'address', 'description' ]
 
 
 class DevelopedPropertyDetailView(generics.RetrieveUpdateDestroyAPIView):
