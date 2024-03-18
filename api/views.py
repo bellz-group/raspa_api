2ca7ac5a548a45bd7b9875b4ee4763aec2908dce
unknown <olaniyigeorge77@gmail.com>
2024-03-18 04:33:08 +0100

    Filter properties with action, size

--- api/views.py
+++ api/views.py
@@ -7,6 +7,7 @@ from api.filters import DevdPropertyFilter
 from .models import *
 from .serializers import *
 from rest_framework import filters
+from django.db.models import Q
 
 class Index(APIView):
     def get(self, request, format=None):
@@ -23,11 +24,36 @@ class DevelopedPropertyListCreateViewSet(generics.ListCreateAPIView):
     serializer_class = DevelopedPropertySerializer
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
     filterset_class = DevdPropertyFilter
-    filterset_fields = ('size', 'type', 'bdrs', 'flrs')
+    filterset_fields = ('size', 'type', 'bdrs', 'flrs', 'actions')
     search_fields = ['property_name', 'address', 'description' ]
+
+    def filter_queryset(self, queryset):
+        queryset = super().filter_queryset(queryset)
+        actions = self.request.query_params.getlist('actions')
+        if actions:
+
+            action_codes = {
+                "NoActions": "000",
+                'rent': ['001', '011', '101', '111'],
+                "sale": ['010', '011', '110','111'],
+                "invest":['100', '101', '110', '111']
+            }
+            # Construct Q objects for filtering properties with the specified actions
+            q_objects = Q()
+            
+            action = actions[0]
+            action_code = action_codes.get(action)
+            if action_code:
+                if isinstance(action_code, list):
+                    q_objects |= Q(actions__in=action_code)
+                else:
+                    q_objects |= Q(actions__contains=action_code)
+            # Filter queryset to include properties that match the specified actions
+            queryset = queryset.filter(q_objects)
+        return queryset
 
 
 class DevelopedPropertyDetailView(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = DevelopedProperty.objects.all()
-    serializer_class = DevelopedPropertySerializer
+    serializer_class = DevelopedPropertyDetailsSerializer
