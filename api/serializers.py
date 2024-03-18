2e67cd65bcea1965761d64628b93b733fd0ffe12
unknown <olaniyigeorge77@gmail.com>
2024-03-18 04:33:48 +0100

    property detail srlzr with features

--- api/serializers.py
+++ api/serializers.py
@@ -2,11 +2,14 @@
 
 
 from rest_framework import serializers
-from .models import DevelopedProperty
+from .models import *
 
 
 
-
+class FeatureSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Feature
+        fields = "__all__"
 
 
 
@@ -15,3 +18,18 @@ class DevelopedPropertySerializer(serializers.ModelSerializer):
     class Meta:
         model = DevelopedProperty
         fields = "__all__"
+
+
+class DevelopedPropertyDetailsSerializer(serializers.ModelSerializer):
+    features = FeatureSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = DevelopedProperty
+        fields = ["id", "property_name", "type", "actions", "description", "owner", "listed_by", "latitude", "longitude", "address", "amenities", "features"]
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        features = instance.features.all()
+        representation['features'] = FeatureSerializer(features, many=True).data
+        return representation
+
