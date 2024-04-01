c85b0e8bccbe3e29812329e8ae86fa2ebe49fcb8
unknown <olaniyigeorge77@gmail.com>
2024-04-01 18:21:43 +0100

    Added actions to fields

--- api/filters.py
+++ api/filters.py
@@ -11,11 +11,11 @@ class DevdPropertyFilter(django_filters.FilterSet):
     size__lte = django_filters.NumberFilter(field_name="size", lookup_expr="lte")
     size__gte = django_filters.NumberFilter(field_name="size", lookup_expr="gte")
     type = django_filters.CharFilter(field_name="type", lookup_expr="icontains")
-    bdrs__lte = django_filters.NumberFilter(field_name="bdrs", lookup_expr="lte")
-    bdrs__gte = django_filters.NumberFilter(field_name="bdrs", lookup_expr="gte")
-    flrs__lte = django_filters.NumberFilter(field_name="flrs", lookup_expr="lte")
-    flrs__gte = django_filters.NumberFilter(field_name="flrs", lookup_expr="gte")
+    # bdrs__lte = django_filters.NumberFilter(field_name="bdrs", lookup_expr="lte")
+    # bdrs__gte = django_filters.NumberFilter(field_name="bdrs", lookup_expr="gte")
+    # flrs__lte = django_filters.NumberFilter(field_name="flrs", lookup_expr="lte")
+    # flrs__gte = django_filters.NumberFilter(field_name="flrs", lookup_expr="gte")
 
     class Meta:
         model = DevelopedProperty
-        fields = ['size__lte', 'size__gte', 'type', 'bdrs__lte', 'bdrs__gte', 'flrs__lte', 'flrs__gte']
+        fields = ['size__lte', 'size__gte', 'type', 'actions']
