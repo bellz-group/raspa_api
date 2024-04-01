82d373be4b97911e7d4fbecd2a400d110032802e
unknown <olaniyigeorge77@gmail.com>
2024-04-01 18:19:01 +0100

    Properties features

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,11 @@ urlpatterns = [
 
     path("", Index.as_view(), name="index"),
 
-    # ---------- BUILDING ----------
+    # ---------- Developped Properties (BUILDINGS) ----------
     path("developed-properties/", DevelopedPropertyListCreateViewSet.as_view(), name="building_list_create"),
     path('developed-properties/<uuid:pk>/', DevelopedPropertyDetailView.as_view(), name="building_details"),
+
+    # ---------- Developed Property Features ---------
+    path('properties/<uuid:pk>/features/', PropertyFeatures.as_view({"get": "list"}), name="property_amenities"),
+    path('feature/<uuid:pk>/', Feature.as_view(), name="property_amenities"),
 ]
