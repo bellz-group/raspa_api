2571497c3483cc765884b28b5ff41ecdd72033f0
unknown <olaniyigeorge77@gmail.com>
2024-04-01 22:30:22 +0100

    Renamed class

--- api/filters.py
+++ api/filters.py
@@ -1,21 +1,17 @@
 import django_filters
-from .models import DevelopedProperty
+from .models import Property
 
 
 
 
-class DevdPropertyFilter(django_filters.FilterSet):
+class PropertyFilter(django_filters.FilterSet):
     '''
     Customize filter to be case insensitive
     '''
     size__lte = django_filters.NumberFilter(field_name="size", lookup_expr="lte")
     size__gte = django_filters.NumberFilter(field_name="size", lookup_expr="gte")
     type = django_filters.CharFilter(field_name="type", lookup_expr="icontains")
-    # bdrs__lte = django_filters.NumberFilter(field_name="bdrs", lookup_expr="lte")
-    # bdrs__gte = django_filters.NumberFilter(field_name="bdrs", lookup_expr="gte")
-    # flrs__lte = django_filters.NumberFilter(field_name="flrs", lookup_expr="lte")
-    # flrs__gte = django_filters.NumberFilter(field_name="flrs", lookup_expr="gte")
-
+    
     class Meta:
-        model = DevelopedProperty
+        model = Property
         fields = ['size__lte', 'size__gte', 'type', 'actions']
